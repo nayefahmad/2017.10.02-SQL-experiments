12085ee736a6bb5bd1198c1b2cff0b2d0b857e9d
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-08-24 16:15:55 -0700

    Q2: return ED rows with patients who exist in ADTC

--- 2018-08-24_subquery-examples.sql
+++ 2018-08-24_subquery-examples.sql
@@ -10,6 +10,7 @@ From "SQL Server TSQL Fundamentals"
 */---------------------------------------------------------
 
 drop table if exists #t1_ed_visits;  
+drop table if exists #t2_adtc;  
 
 -- let's create a table to play with 
 select * 
@@ -19,9 +20,15 @@ where FacilityShortName = 'LGH'
 	and StartDate = '2018-01-01' 
 
 
+select * 
+into #t2_adtc
+from ADTCMart.ADTC.AdmissionDischargeView
+where AdmissionFacilityLongName = 'LIONS GATE HOSPITAL' 
+	and AdjustedAdmissionDate between '2018-01-01' and '2018-01-07' 
 
-
+---------------------------------------------------------------------
 -- Q1 Return info on the visit with max age
+---------------------------------------------------------------------
 
 -- Solution 1: using top 1 
 -- You can't just use max(age) and group by patientID. That would give e.g. 
@@ -62,6 +69,40 @@ select patientId, age
 from #t1_ed_visits
 where age = (select max(age) 
 			 from #t1_ed_visits) 
+
+
+
+
+
+
+
+
+
+
+
+---------------------------------------------------------------------
+-- Question 2: return rows with patients who exist in ADTC 
+---------------------------------------------------------------------
+
+-- Solution 1: using join 
+select t1.patientid, t1.age
+from #t1_ed_visits t1
+	inner join #t2_adtc t2
+		on t1.patientid =  t2.patientid
+order by t1.age desc 
+
+--Solution 2: using sub-query 
+select t1.patientid, t1.age
+from #t1_ed_visits t1
+where t1.patientID in (select patientID 
+					   from #t2_adtc) 
+order by age desc  
+
+
+
+
+
+
 
 
 
