383a612f2020a4a08ab83a10207d2140344f14f4
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-09-13 19:22:58 -0700

    pulling top 2 scores per student - Notes on self-joins,

--- 2019-08-21_transact-sql-cookbook-examples.sql
+++ 2019-08-21_transact-sql-cookbook-examples.sql
@@ -7,7 +7,7 @@ Example from 'Transact SQL Cookbook'
 Nayef 
 
 Let's say your grade will be based only on your top 2 term papers. 
-Write a query that pull top 2 scores for each student 
+Write a query that pulls top 2 scores for each student 
 
 */----------------------------------------------------------------------
 
@@ -56,7 +56,8 @@ Here, we take each {student, course, termpaper, max(score)} row from the
 outer query, and we compare it with the list of 2 rows of {score} in the 
 inner query. 
 
-Only if the score in the outer query matches with one of hte top 2 
+Only if the score in the outer query matches with one of the top 2 will it
+be returned
 
 
 */------------------------------------------------------------
@@ -72,7 +73,7 @@ where s1.score IN (
 	-- by the sub-query, then that row is deleted from the outer 
 	-- query 
 	SELECT TOP 2 s2.Score 
-    FROM #Students s2
+    FROM #Students s2  -- note that the sub-query is from the same table as the outer query 
     WHERE s2.CourseId = s1.CourseId AND    -- think of replacing the reference to s1.CourseID with the value of courseID in the current row of the outer query that is under consideration
           s2.StudentName = s1.StudentName
 	order by s2.Score desc)
@@ -89,7 +90,7 @@ Solution 2:
 This is more general across diff versions of SQL 
 */------------------------------------------------------------
 
-SELECT s1.StudentName,s1.CourseId, s1.TermPaper, MAX(s1.Score) Score
+SELECT s1.StudentName, s1.CourseId, s1.TermPaper, MAX(s1.Score) Score
 FROM #Students s1 INNER JOIN #Students s2
    ON s1.CourseId=s2.CourseId AND
       s1.StudentName=s2.StudentName
@@ -97,5 +98,39 @@ GROUP BY s1.CourseId, s1.StudentName, s1.TermPaper
 HAVING SUM(CASE WHEN s1.Score <= s2.Score THEN 1 END) <= 2
 ORDER BY s1.StudentName, s1.CourseId, Score DESC
 
+-- Recall that the logical order of SQL clauses is: from, where, groupby, having, select 
+
+-- That's why the MAX( ) in the SELECT clause only operates on the top 2 scores, 
+-- instead of all scores. 
 
 
+-- Let's take a closer look at what that self-join does, without aggregating: 
+select * from #Students order by StudentName, CourseId, TermPaper; 
+
+select s1.StudentName as s1_name
+	, s1.CourseId as s1_course
+	, s1.TermPaper as s1_paper
+	, s1.Score as s1_score
+	
+	, s2.StudentName as s2_name
+	, s2.CourseId as s2_course
+	, s2.TermPaper as s2_paper
+	, s2.Score as s2_score 
+
+FROM #Students s1 INNER JOIN #Students s2
+   ON s1.CourseId=s2.CourseId AND
+      s1.StudentName=s2.StudentName
+order by s1_name, s1_course, s1_paper
+	, s2_name, s2_course, s2_paper
+
+-- Note that the inner self-join is like a cross-join within groups defined 
+-- by the join conditions. E.g. Cross join within Course/Student_Name groups 
+
+-- Here's what the cross self-join does: 
+/*
+select s1.*, s2.* 
+from #Students s1 
+cross join #Students s2
+order by s1.StudentName, s1.CourseId, s1.TermPaper
+	, s2.StudentName, s2.CourseId, s2.TermPaper
+*/
