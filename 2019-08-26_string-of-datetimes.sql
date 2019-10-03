58bea5b86e032b85f2cce6f850c04f67cafe89de
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-10-03 16:32:22 -0700

    fix issues with duplicates; SUCCESS

--- 2019-08-26_string-of-datetimes.sql
+++ 2019-08-26_string-of-datetimes.sql
@@ -14,24 +14,20 @@ TODO:
 
 ********************************************************************/
 
-
-
-
 drop table if exists #datetimes
 GO  
 -- this GO statement ensures that the first thing SQL does is drop the table. This is necessary 
 -- for the rest of the program to work
 
-declare @startdate datetime = '2017-03-28 00:00:00.00' 
+declare @startdate datetime = '2017-05-28 00:00:00.00' 
 declare @horizon_in_minutes int = 1440  -- 4 weeks
-declare @site varchar(50) = 'Lions Gate Hospital' 
+declare @site varchar(50) = 'Richmond Hospital' 
 
 -- create table with a column of datetimes 
 create table #datetimes (datetimes_col datetime) 
 
 insert into #datetimes values 
 	(@startdate) 
-
 -- select * from #datetimes
 
 
@@ -43,7 +39,6 @@ begin
 	insert into #datetimes values (DATEADD(mi, @counter, @startdate))
 	set @counter = @counter + 1
 end 
-
 --select * from #datetimes order by datetimes_col
 
 
@@ -61,11 +56,10 @@ set entry_count = (select count(PatientID)
 
 				   group by CensusDate) 
 where datetimes_col = @startdate 
-
 --select * from #datetimes order by datetimes_col
 
 
--- add site 
+-- add site column 
 alter table #datetimes
 add site varchar(25)
 
@@ -76,117 +70,118 @@ set site = @site
 
 
 /********************************************************************************
-Join datetime col to get full list of admits and discharges by minute
+Pull and group admits and discharges 
 *********************************************************************************/
--- now join on ADTC.admissiondischarge: 
-drop table if exists #t4_add_admits_discharges; 
+-- admits
+drop table if exists #t2_admits_grouped
+select AdjustedAdmissionDate
+	, AdjustedAdmissionTime
+	-- , (AdjustedAdmissionDate + AdjustedAdmissionTime) as admit_date_time
+	, count(*) as num_admits
+into #t2_admits_grouped
+from ADTCMart.ADTC.AdmissionDischargeView 
+where 1=1
+	and AdmissionFacilityLongName = @site
+	and AdjustedAdmissionDate = @startdate
+group by AdjustedAdmissionDate
+	, AdjustedAdmissionTime
+order by AdjustedAdmissionDate
+	, AdjustedAdmissionTime
+-- select * from #t2_admits_grouped order by AdjustedAdmissionDate, AdjustedAdmissionTime
+
+-- discharges
+drop table if exists #t3_disch_grouped
+select AdjustedDischargeDate
+	, AdjustedDischargeTime
+	, count(*) as num_disch
+into #t3_disch_grouped
+from ADTCMart.ADTC.AdmissionDischargeView 
+where 1=1
+	and DischargeFacilityLongName = @site
+	and AdjustedDischargeDate = @startdate
+group by AdjustedDischargeDate
+	, AdjustedDischargeTime
+order by AdjustedDischargeDate
+	, AdjustedDischargeTime
+-- select * from #t3_disch_grouped order by AdjustedDischargeDate, AdjustedDischargeTime
+
+
+/********************************************************************************
+Join datetimes table with admits and discharges 
+*********************************************************************************/
+
+drop table if exists #t4_admits_and_disch
 select t1.datetimes_col
-	, t1.[site]
+	, t1.site
+	, t2.AdjustedAdmissionDate
+	, t2.AdjustedAdmissionTime
 
-	, t2_ad.PatientId as admit_pt_id
-	, t2_ad.AdjustedAdmissionDate
-	, t2_ad.AdjustedAdmissionTime
+	, t3.AdjustedDischargeDate
+	, t3.AdjustedDischargeTime
 
-	, t3_ad.PatientId as disch_pt_id
-	, t3_ad.AdjustedDischargeDate
-	, t3_ad.AdjustedDischargeTime
+	, t2.num_admits
+	, t3.num_disch
 	, t1.entry_count
 	, t1.exit_count
-into #t4_add_admits_discharges
+into #t4_admits_and_disch
 from #datetimes t1
-	LEFT JOIN ADTCMart.ADTC.AdmissionDischargeView t2_ad
-		ON t1.datetimes_col = (t2_ad.AdjustedAdmissionDate + t2_ad.AdjustedAdmissionTime)
-		and t1.site = t2_ad.[AdmissionFacilityLongName]
-		-- todo: here you can add conditions for nursing unit code, admitting doctor type, etc.
+	left join #t2_admits_grouped t2
+	on t1.datetimes_col = (t2.AdjustedAdmissionDate + t2.AdjustedAdmissionTime) 
 
-	left join ADTCMart.ADTC.AdmissionDischargeView t3_ad
-		on t1.datetimes_col = (t3_ad.AdjustedDischargeDate + t3_ad.AdjustedDischargeTime)
-		and t1.site = t3_ad.DischargeFacilityLongName
-		-- todo: here you can add conditions for nursing unit code, admitting doctor type, etc.
+	left join #t3_disch_grouped t3
+	on t1.datetimes_col = (t3.AdjustedDischargeDate + t3.AdjustedDischargeTime) 
+-- select * from #t4_admits_and_disch order by datetimes_col
 
-order by t1.datetimes_col
+update #t4_admits_and_disch
+set entry_count = isnull(entry_count, 0) + isnull(num_admits, 0)
 
-select * from #t4_add_admits_discharges order by datetimes_col
+update #t4_admits_and_disch
+set exit_count = isnull(num_disch, 0) * -1
 
-/*********************************************************************************
-Code queue operations: enqueue and dequeue 
-*********************************************************************************/
+alter table #t4_admits_and_disch
+add total_change int 
 
--- code every row with an admit as +1 in [entry_count]
-update #t4_add_admits_discharges
-set entry_count = 1 
-where AdjustedAdmissionDate is not null 
+update #t4_admits_and_disch
+set total_change = entry_count + exit_count
 
--- code every row with an discharge as +1 in [exit_count]
-update #t4_add_admits_discharges
-set exit_count = -1
-where AdjustedDischargeDate is not null 
-
--- select * from #t4_add_admits_discharges order by datetimes_col
-
--- deal with duplicate rows:
-drop table if exists #t4_1_grouped; 
-select datetimes_col
-	, [site]
-	, AdjustedAdmissionDate
-	, AdjustedAdmissionTime
-	, AdjustedDischargeDate
-	, AdjustedDischargeTime
-	, sum(entry_count) as entry_count
-	, sum(exit_count) as exit_count
-into #t4_1_grouped 
-from #t4_add_admits_discharges
-group by datetimes_col
-	, [site]
-	, AdjustedAdmissionDate
-	, AdjustedAdmissionTime
-	, AdjustedDischargeDate
-	, AdjustedDischargeTime
-order by datetimes_col
-
--- select * from #t4_1_grouped order by datetimes_col
-
-
-
--- find net changes to queue by minute: 
-drop table if exists #t5_add_net_changes
-select *
-	, (isnull(entry_count, 0) + isnull(exit_count, 0)) as net_change
-into #t5_add_net_changes
-from #t4_1_grouped
-order by datetimes_col
-
---select * from #t4_add_admits_discharges order by datetimes_col
---select * from #t5_add_net_changes order by datetimes_col
+-- select * from #t4_admits_and_disch order by datetimes_col
 
 
 
 
 -- use correlated subquery to find running total 
-drop table if exists #t6_census_by_minute
-select t5_1.*
-	, (select sum(net_change) 
-		from #t5_add_net_changes t5_2
-		where t5_2.datetimes_col <= t5_1.datetimes_col
+drop table if exists #t5_census_by_minute
+select t4.datetimes_col
+	, t4.site
+	, t4.AdjustedAdmissionDate
+	, t4.AdjustedAdmissionTime
+	, t4.AdjustedDischargeDate
+	, t4.AdjustedDischargeTime
+	, t4.entry_count
+	, t4.exit_count
+	, t4.total_change
+	, (select sum(total_change) 
+		from #t4_admits_and_disch t4_2
+		where t4_2.datetimes_col <= t4.datetimes_col
 	) as census_minute_level
 
-into #t6_census_by_minute
-from #t5_add_net_changes t5_1
+into #t5_census_by_minute
+from #t4_admits_and_disch t4
 order by datetimes_col
 
 -- view: 
--- select * from #t6_census_by_minute order by datetimes_col
+select * from #t5_census_by_minute order by datetimes_col
 
 
 /*********************************************************************************
 Validation with ADTC CensusView
 *********************************************************************************/
-/*
+
 -- ending figure from my calculation: 
 select census_minute_level 
 	, datetimes_col
-from #t6_census_by_minute
-where datetimes_col = (select max(datetimes_col) from #t5_add_net_changes)
+from #t5_census_by_minute
+where datetimes_col = (select max(datetimes_col) from #t5_census_by_minute)
 
 -- check ending figure with census view figure: 
 select count(PatientID) as census_from_ADTC
@@ -211,9 +206,9 @@ order by CensusDate
 -- Why is the figure higher than it should be? Are we not counting all 
 -- discharges properly? 
 
-select sum(entry_count) - (select entry_count from #t6_census_by_minute where datetimes_col = @startdate)as [sum_entry_count]
+select sum(entry_count) - (select entry_count from #t5_census_by_minute where datetimes_col = @startdate)as [sum_entry_count]
 	, sum(exit_count) as [sum_exit_count]
-from #t5_add_net_changes
+from #t5_census_by_minute
 -- 114 discharges 
 
 select count(*) as disch_from_adtc
@@ -223,7 +218,7 @@ where 1=1
 	and AdjustedDischargeDate = @startdate 
 -- 114 discharges 
 
-*/
+
 ------------------------------------------------------------------
 -- Are we counting too many admits?? 
 select count(*) as admit_from_adtc
@@ -233,11 +228,15 @@ where 1=1
 	and AdjustedAdmissionDate = @startdate 
 
 -- disaggregated: 
-select PatientId
-	, AdjustedAdmissionDate
-	, AdjustedAdmissionTime
-from ADTCMart.ADTC.AdmissionDischargeView
-where 1=1 
-	and AdmissionFacilityLongName = @site
-	and AdjustedAdmissionDate = @startdate 
-order by AdjustedAdmissionDate, AdjustedAdmissionTime
+--select PatientId
+--	, AdjustedAdmissionDate
+--	, AdjustedAdmissionTime
+--from ADTCMart.ADTC.AdmissionDischargeView
+--where 1=1 
+--	and AdmissionFacilityLongName = @site
+--	and AdjustedAdmissionDate = @startdate 
+--order by AdjustedAdmissionDate, AdjustedAdmissionTime
+
+
+
+
