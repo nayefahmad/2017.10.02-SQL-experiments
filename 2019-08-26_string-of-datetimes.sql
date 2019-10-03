a99e36e176ffd904caede06c6e054c806511743c
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2019-10-03 13:10:14 -0700

    validation: checking numbers against ADTC CensusView

--- 2019-08-26_string-of-datetimes.sql
+++ 2019-08-26_string-of-datetimes.sql
@@ -20,7 +20,7 @@ GO
 -- this GO statement ensures that the first thing SQL does is drop the table. This is necessary 
 -- for the rest of the program to work
 
-declare @startdate datetime = '2018-05-28 00:00:00.00' 
+declare @startdate datetime = '2019-09-28 00:00:00.00' 
 declare @horizon_in_minutes int = 1440  -- 4 weeks
 
 -- create table with a column of datetimes 
@@ -50,7 +50,7 @@ alter table #datetimes
 add entry_count int, exit_count int
 
 update #datetimes
-set entry_count = (select count(*) 
+set entry_count = (select count(PatientID) 
 				   from ADTCMart.ADTC.CensusView
 				   where FacilityLongName = 'Richmond Hospital' 
 						and CensusDate = @startdate
@@ -117,7 +117,28 @@ where AdjustedDischargeDate is not null
 
 -- select * from #t4_add_admits_discharges order by datetimes_col
 
--- TODO: do we have to deal with duplicate rows?? 
+-- deal with duplicate rows:
+drop table if exists #t4_1_grouped; 
+select datetimes_col
+	, [site]
+	, AdjustedAdmissionDate
+	, AdjustedAdmissionTime
+	, AdjustedDischargeDate
+	, AdjustedDischargeTime
+	, sum(entry_count) as entry_count
+	, sum(exit_count) as exit_count
+into #t4_1_grouped 
+from #t4_add_admits_discharges
+group by datetimes_col
+	, [site]
+	, AdjustedAdmissionDate
+	, AdjustedAdmissionTime
+	, AdjustedDischargeDate
+	, AdjustedDischargeTime
+order by datetimes_col
+
+-- select * from #t4_1_grouped order by datetimes_col
+
 
 
 -- find net changes to queue by minute: 
@@ -125,21 +146,80 @@ drop table if exists #t5_add_net_changes
 select *
 	, (isnull(entry_count, 0) + isnull(exit_count, 0)) as net_change
 into #t5_add_net_changes
-from #t4_add_admits_discharges
+from #t4_1_grouped
 order by datetimes_col
 
--- select * from #t5_add_net_changes order by datetimes_col
+--select * from #t4_add_admits_discharges order by datetimes_col
+--select * from #t5_add_net_changes order by datetimes_col
 
 
 
 
 -- use correlated subquery to find running total 
+drop table if exists #t6_census_by_minute
 select t5_1.*
 	, (select sum(net_change) 
 		from #t5_add_net_changes t5_2
 		where t5_2.datetimes_col <= t5_1.datetimes_col
 	) as census_minute_level
 
+into #t6_census_by_minute
 from #t5_add_net_changes t5_1
 order by datetimes_col
 
+-- view: 
+select * from #t6_census_by_minute order by datetimes_col
+
+
+/*********************************************************************************
+Validation with ADTC CensusView
+*********************************************************************************/
+
+-- ending figure from my calculation: 
+select census_minute_level 
+	, datetimes_col
+from #t6_census_by_minute
+where datetimes_col = (select max(datetimes_col) from #t5_add_net_changes)
+
+-- check ending figure with census view figure: 
+select count(PatientID) as census_from_ADTC
+	, CensusDate
+from ADTCMart.ADTC.CensusView
+where FacilityLongName = 'Richmond Hospital' 
+	and CensusDate between @startdate and dateadd(mi, @horizon_in_minutes, @startdate) 
+group by CensusDate
+order by CensusDate
+
+-- hardcoded: 
+--select count(*) as census_from_ADTC
+--	, CensusDate
+--from ADTCMart.ADTC.CensusView
+--where FacilityLongName = 'Richmond Hospital' 
+--	and CensusDate between '2018-05-27' and '2018-05-30' 
+--group by CensusDate
+--order by CensusDate
+
+
+
+-- Why is the figure higher than it should be? Are we not counting all 
+-- discharges properly? 
+
+select sum(entry_count) - (select entry_count from #t6_census_by_minute where datetimes_col = @startdate)as [sum_entry_count]
+	, sum(exit_count) as [sum_exit_count]
+from #t5_add_net_changes
+-- 114 discharges 
+
+select count(*) as disch_from_adtc
+from ADTCMart.ADTC.AdmissionDischargeView
+where 1=1 
+	and DischargeFacilityLongName = 'Richmond Hospital' 
+	and AdjustedDischargeDate = @startdate 
+-- 114 discharges 
+
+
+-- Are we counting too many admits?? 
+select count(*) as admit_from_adtc
+from ADTCMart.ADTC.AdmissionDischargeView
+where 1=1 
+	and AdmissionFacilityLongName = 'Richmond Hospital' 
+	and AdjustedAdmissionDate = @startdate 
